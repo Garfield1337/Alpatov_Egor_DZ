ff69ae7f90883e60aeca3e668b80062550229e64
Garfield1337 <salkovadarya2002@gmail.com>
2024-10-18 19:18:24 +0700

    hello, world

--- lecture_2/func2.py
+++ lecture_2/func2.py
@@ -16,29 +16,14 @@ def str_num_1(number):
                     ['одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'],
                     ['двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто']]
     if 1 <= number <= 10:
-        return str(complex_list[0][number - 1]).title()
+        return complex_list[0][number - 1].title()
     elif 11 <= number <= 19:
         second_part = int(str(number)[1])
-        return str(complex_list[1][second_part - 1]).title()
+        return complex_list[1][second_part - 1].title()
     elif 20 <= number <= 99:
         first_part = int(str(number)[0])
         second_part = int(str(number)[1])
         if second_part == 0:
-            return str(complex_list[2][first_part - 2]).title()
+            return complex_list[2][first_part - 2].title()
         else:
-            return str(f'{complex_list[2][first_part - 2].title()} {complex_list[0][second_part - 1]}')
-
-def str_num_2(number):
-    complex_list = [['один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять', 'десять'],
-                    ['одиннадцать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'],
-                    ['двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто']]
-    number = check_number(number)
-    if 1 <= number <= 10:
-        return str(complex_list[0][number - 1]).title()
-    elif 11 <= number <= 19:
-        return str(complex_list[1][int(str(number)[1]) - 1]).title()
-    elif 20 <= number <= 99:
-        if int(str(number)[1]) == 0:
-            return str(complex_list[2][int(str(number)[0]) - 2]).title()
-        else:
-            return str(f'{complex_list[2][int(str(number)[0]) - 2].title()} {complex_list[0][int(str(number)[1]) - 1]}')
+            return complex_list[2][first_part - 2].title() + ' ' + complex_list[0][second_part - 1]
