d1ac950d47b9441bfa542f0d5f88f4d27a4a0f1d
Garfield1337 <salkovadarya2002@gmail.com>
2024-10-22 12:01:07 +0700

    hello, world

--- lecture_2/pet2.py
+++ lecture_2/pet2.py
@@ -1,14 +1,9 @@
-from time import sleep as sl
 from func2 import str_num_1
 
 while True:
     number = input('Введите число от 1 до 99 или введите "N", '
                    'чтобы завершить выполнение программы.\n').lower()
     if number == 'n':
-        print('\nЗавершение работы программы...\n', flush=True)
-        sl(3)
-        print('  /\_/\\\n / @ @ \\\n( ~~0~~ )', flush=True)
-        sl(1)
         quit()
     else:
         print(f'\n{str_num_1(number)}', end='\n\n')
