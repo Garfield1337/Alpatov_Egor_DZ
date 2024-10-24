b5559a868d3342b536a060e5420b8ea6050276f6
Garfield1337 <salkovadarya2002@gmail.com>
2024-10-25 00:15:56 +0700

    hello, world

--- lecture_3/pet3.py
+++ lecture_3/pet3.py
@@ -2,5 +2,5 @@ from func_3 import temp_switch
 
 
 while True:
-    temp_in = input('Введите значение температуры в градусах (C) или фаренгейтах (F).\n').lower()
-    print(temp_switch(temp_in))
+    temp_input = input('Введите значение температуры в градусах (C) или фаренгейтах (F).\n').lower()
+    print(temp_switch(temp_input))
